7993ad938f36387f3b3855d7197da659b89e1aa7
Jlupuka <dima.usachev.0790@mail.ru>
2024-04-20 19:40:37 +0500

    Completed <<Таймер-бот 2.0>>

--- handlers/message/a_users/user/commands.py
+++ handlers/message/a_users/user/commands.py
@@ -1,19 +1,33 @@
-from aiogram import Router
-from aiogram.types import Message
-from aiogram.filters import Command
+import asyncio
+import re
+from re import Match
 
-from lexicon.ru.lexicon import CommandText
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import Command, CommandObject
+
+from lexicon.ru.lexicon import CommandText, ErrorText
 
 from service.scriptsService.reformat_time import TimeService
 
 router: Router = Router()
 
 
-@router.message(Command("time"))
+@router.message(Command(commands=["time"]))
 async def command_time(message: Message) -> None:
     await message.answer(text=CommandText.nowTime.format(nowTime=await TimeService.now_time()))
 
 
-@router.message(Command("date"))
+@router.message(Command(commands=["date"]))
 async def command_date(message: Message) -> None:
     await message.answer(text=CommandText.nowDate.format(nowDate=await TimeService.now_date()))
+
+
+@router.message(Command(commands=["set_timer"]))
+async def command_set_timer(message: Message, command: CommandObject) -> Message:
+    if command.args and re.match(r"^-?\d*\.?\d+$", command.args):
+        if 0 < (sleep_time := float(command.args)) < 1000:
+            await asyncio.sleep(sleep_time)
+            return await message.answer(text=CommandText.setTimer.format(sleepTime=sleep_time))
+        return await message.answer(text=ErrorText.rangeTimer)
+    return await message.answer(text=ErrorText.typeTimer)
